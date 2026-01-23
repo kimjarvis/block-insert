952a2616ff0994069524fddd8199c0bcd64b187f
kimjarvis <kim.jarvis@tpfsystems.com>
2026-01-23 18:11:35 +0000

    Add tests

---
+++ tests/testdata/mutable/insertion.py
@@ -0,0 +1,21 @@
+# Test block-insert
+
+# block insert source.md
+
+# block insert source.md 4
+
+    # block insert source.md
+
+    # block insert source.md 4
+
+    # block insert source.md -4
+
+# block insert source.md -4
+
+# block insert indented.md 
+
+# block insert indented.md 4
+
+# block insert indented.md -4
+
+# block insert source.md 0 more text

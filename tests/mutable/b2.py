d54907f270e72d0065be32b4edf420040872c0e0
kimjarvis <kim.jarvis@tpfsystems.com>
2026-01-28 11:34:07 +0000

    block extract

---
+++ tests/mutable/b2.py
@@ -0,0 +1,4 @@
+  def example_function():
+      print("This is an example function. 1")
+      pass
+ 
---
+++ tests/mutable/b4.py
@@ -0,0 +1,3 @@
+         def example_function():
+             print("This is an example function. 2")
+             pass
